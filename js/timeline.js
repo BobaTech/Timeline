7572254b78c9ed86720ebb7296c574ddebd742b9
CajunAzn7 <kevinw326@gmail.com>
2015-04-01 18:01:43 +0000

    still not workin

--- js/timeline.js
+++ js/timeline.js
@@ -1,4 +1,7 @@
   $(function() {
     $( "#slider" ).slider();
+    $("#slider").css("margin-top", "85%");
+    $("#slider").css("margin-left", "15%");
+    $("#slider").css("position", "absolute");
   });
   
