701df88cb2143300599da9d259bc8480cdcc84a2
CajunAzn7 <kevinw326@gmail.com>
2015-04-02 01:27:02 +0000

    Working when not on koding

--- js/timeline.js
+++ js/timeline.js
@@ -1,7 +1,4 @@
   $(function() {
     $( "#slider" ).slider();
-    $("#slider").css("margin-top", "85%");
-    $("#slider").css("margin-left", "15%");
-    $("#slider").css("position", "absolute");
   });
   
