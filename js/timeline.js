b6db591034320ec79f2be32f4c5505bccc9f0a3f
CajunAzn7 <kevinw326@gmail.com>
2015-04-03 02:20:15 +0000

    working slider

--- js/timeline.js
+++ js/timeline.js
@@ -1,12 +1,16 @@
   
 $(function(){
     $("#submit").click(function(){
-        $( ".input" ).hide();
-        $("#slider").show();
-        $( "#slider" ).slider({
-            min: document.getElementById('start'),
-            max: document.getElementById('end')
-        });
-        
+        if(!isNaN(document.getElementById("start").value) && !isNaN(document.getElementById("end").value) && document.getElementById("end").value >= document.getElementById("start").value){
+            $( ".input" ).fadeOut();
+            $("#slider").show();
+            $( "#slider" ).slider({
+                min: document.getElementById("start").value,
+                max: document.getElementById("end").value
+            });
+        }
+        else{
+            alert("Please enter valid values for both the start and end year");
+        }
     });
 });
