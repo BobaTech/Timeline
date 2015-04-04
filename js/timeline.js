45d3f77562b32254a77b4110790d20f4c7af0ff8
CajunAzn7 <kevinw326@gmail.com>
2015-04-04 04:43:38 +0000

    working slider, working on timeline

--- js/timeline.js
+++ js/timeline.js
@@ -1,22 +1,17 @@
-function createSlider(name ,a, b){
-    $( name ).slider({
-        min: a,
-        max: b
-    });
-}
 $(function(){
     $("#submit").click(function(){
         var a = document.getElementById("start").value;
         var b = document.getElementById("end").value;
-        if(!isNaN(a) && !isNaN(b) && b >= a && a.length !== 0  && b.length !== 0){
+        if(!isNaN(a) && !isNaN(b) && a.length !== 0  && b.length !== 0){
             $( ".input" ).fadeOut();
-            createSlider("#slider", a, b);
-            $("#slider").show();
+            $( "#slider" ).slider({
+                min: $("start").val(),
+                max: $("end").val()
+             });
+            $( "#slider" ).fadeIn();
         }
         else{
             alert("Please enter valid values for both the start and end year");
         }
     });
 });
-
-
