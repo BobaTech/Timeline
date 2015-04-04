78a3ecbfa3002d2bce5c44c5e5edd83fa305d5d1
CajunAzn7 <kevinw326@gmail.com>
2015-04-04 03:02:05 +0000

    broken slider

--- js/timeline.js
+++ js/timeline.js
@@ -1,16 +1,22 @@
-  
+function createSlider(name ,a, b){
+    $( name ).slider({
+        min: a,
+        max: b
+    });
+}
 $(function(){
     $("#submit").click(function(){
-        if(!isNaN(document.getElementById("start").value) && !isNaN(document.getElementById("end").value) && document.getElementById("end").value >= document.getElementById("start").value){
+        var a = document.getElementById("start").value;
+        var b = document.getElementById("end").value;
+        if(!isNaN(a) && !isNaN(b) && b >= a && a.length !== 0  && b.length !== 0){
             $( ".input" ).fadeOut();
+            createSlider("#slider", a, b);
             $("#slider").show();
-            $( "#slider" ).slider({
-                min: document.getElementById("start").value,
-                max: document.getElementById("end").value
-            });
         }
         else{
             alert("Please enter valid values for both the start and end year");
         }
     });
 });
+
+
