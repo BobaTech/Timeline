c171165d0be48b831e680b9a700f21da8cb4f634
CajunAzn7 <kevinw326@gmail.com>
2015-04-08 05:07:16 +0000

    Working slider w/ input

--- js/timeline.js
+++ js/timeline.js
@@ -1,14 +1,25 @@
-$(function(){
+function createSlider(name, a, b){
+    $(name).slider({
+        value: parseInt(a, 10),
+        min: parseInt(a, 10),
+        max: parseInt(b, 10)
+    });
+}
+
+$(document).ready(function(){
     $("#submit").click(function(){
-        var a = document.getElementById("start").value;
-        var b = document.getElementById("end").value;
+        var a = document.getElementById( "start" ).value;
+        var b = document.getElementById( "end" ).value;
         if(!isNaN(a) && !isNaN(b) && a.length !== 0  && b.length !== 0){
-            $( ".input" ).fadeOut();
+            createSlider("#slider", a, b);
             $( "#slider" ).slider({
-                min: $("start").val(),
-                max: $("end").val()
-             });
+                slide: function( event, ui ) {
+                    $( "#year" ).val( ui.value );
+                }
+            });
+            $( "#year" ).val($( "#slider" ).slider( "value" ) );
             $( "#slider" ).fadeIn();
+            $( ".input" ).fadeOut();
         }
         else{
             alert("Please enter valid values for both the start and end year");
